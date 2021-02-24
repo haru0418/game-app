5f96f67f700850f0367a3f54ebe200f6b22d58e1
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-24 14:55:54 +0900

    バリデーションとアソシエーション記述

--- app/models/game_title.rb
+++ app/models/game_title.rb
@@ -1,2 +1,12 @@
 class GameTitle < ApplicationRecord
+
+  with_options presence: true do
+    validates :image
+    validates :game_title
+    validates :game_info
+    validates :recommended_pc
+    validates :monthly_fee
+  end
+
+  belongs_to :user
 end
