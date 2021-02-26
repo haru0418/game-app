3b566d31c36174b70dcab73d77a228f0d509d086
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-02-26 18:52:06 +0900

    コメント保存機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,14 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to game_title(@comment.game_title)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, game_title_id: params[:game_title_id] )
   end
 end
