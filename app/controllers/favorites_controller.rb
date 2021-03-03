025ce8c66806178aeaa314a7789646f4ba49c977
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-03 23:32:28 +0900

    コントローラー記述

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+
+  def create
+    @game_title = GameTitle.find(params[:game_title_id])
+    @favorite = current_user.favorites.create(game_title_id: params[:game_title_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @game_title = GameTitle.find(params[:game_title_id])
+    @favorite = current_user.favorites.find_by(game_title_id: @game_title.id)
+    @favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
