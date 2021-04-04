e76ac89e3bbbee3bfbe4fa29850afe75a2c20f3b
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-04-05 07:09:52 +0900

    game_titleモデルの単体テストコード

--- spec/models/game_title_spec.rb
+++ spec/models/game_title_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe GameTitle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @game_title = FactoryBot.build(:game_title)
+  end
+
+  describe '投稿できる場合' do
+    it 'すべての情報があれば登録できる' do
+      expect(@game_title).to be_valid
+    end
+  end
+
+  describe '投稿できない場合' do
+    it 'imageがなければ投稿できない' do
+      @game_title.image = nil
+      @game_title.valid?
+      expect(@game_title.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'game_titleがなければ投稿できない' do
+      @game_title.game_title = nil
+      @game_title.valid?
+      expect(@game_title.errors.full_messages).to include("Game title can't be blank")
+    end
+    it 'game_infoがなければ投稿できない' do
+      @game_title.game_info = nil
+      @game_title.valid?
+      expect(@game_title.errors.full_messages).to include("Game info can't be blank")
+    end
+    it 'recommended_pcがなければ投稿できない' do
+      @game_title.recommended_pc = nil
+      @game_title.valid?
+      expect(@game_title.errors.full_messages).to include("Recommended pc can't be blank")
+    end
+    it 'monthly_feeがなければ投稿できない' do
+      @game_title.monthly_fee = nil
+      @game_title.valid?
+      expect(@game_title.errors.full_messages).to include("Monthly fee can't be blank")
+    end
+  end
 end
