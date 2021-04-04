064c2284693081354ea7a2dc501ffabf1ec7e265
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-04-05 06:48:57 +0900

    game_titleモデルのfactorybot記述

--- spec/factories/game_titles.rb
+++ spec/factories/game_titles.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :game_title do
-    
+    game_title {'テスト'}
+    game_info {'テスト'}
+    recommended_pc {'テスト'}
+    monthly_fee {'テスト'}
+    association :user
+
+    after(:build) do |game_title|
+      game_title.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
