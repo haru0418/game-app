115cca412a2a259ce5208bd5c062850efe184439
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-04-18 22:34:56 +0900

    コメント

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメントできる場合' do
+    it 'ユーザーがログインしている時はコメントができる' do
+      expect(@comment).to be_valid
+    end
+  end
 end
